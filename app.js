757c4a3fd12d357cb80c6c014b924969d1b41096
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-03-09 02:10:15 +0530

    🐛 Start script fix

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const app = express();
 const csv = require('csv-parser');
 const fs = require('fs');
 const twilio = require('twilio');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+console.log(process.env.TWILIO_AUTH);
