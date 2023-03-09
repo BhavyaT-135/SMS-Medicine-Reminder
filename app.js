d466f144a6f3843d052222805a624e5081c93953
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-03-09 12:03:42 +0530

    ✅ Testing the twilio messaging api

--- app.js
+++ app.js
@@ -7,4 +7,19 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-console.log(process.env.TWILIO_AUTH);
+app.get('/send-reminders', (req, res) => {
+    const accountSid = process.env.TWILIO_SID;
+    const authToken = process.env.TWILIO_AUTH;
+    const client = twilio(accountSid, authToken);
+    
+    client.messages.create({
+          to: '+918287810214',
+          from: process.env.TWILIO_PHONE,
+          body: "This is a test message for Twilio!"
+      })
+      .then(
+        console.log("Message sent successfully")
+      );
+});
+
+app.listen(3000, console.log(`Server is listening on port 3000...`))
